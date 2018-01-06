654a4a95115313abda344e160c864e0b887dcf1c
kyle chen <chenhaox@outlook.com>
2018-01-06 13:37:24 +0800

    second version

--- work1.js
+++ work1.js
@@ -13,6 +13,7 @@ function readLine (line) {
   if (line !== "\n") {
     var a = parseInt(line.toString().split(' ')[0], 10); 		//parseInt解析字符串
     var b = parseInt(line.toString().split(' ')[1], 10);		
+    var b = parseInt(line.toString().split(' ')[1], 10);							
     console.log(a + b);
     process.exit();
   }
